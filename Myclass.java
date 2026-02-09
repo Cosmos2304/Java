215e28b4af17bc0f744420ebfc3b8235c4bcf0c4
Cosmos2304 <madsdechow1999@gmail.com>
2026-02-09 09:50:19 +0000

    Making first commit

---
+++ Myclass.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MyClass {
+  public static void main(String args[]) {
+    
+    
+    
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Länge a: ");
+    float a =sc.nextFloat();
+    System.out.println("Breite:b  ");
+    float b= sc.nextFloat();
+    A=a*b;
+   
+
+    System.out.println("FLäche = " + A);
+  }
+}
